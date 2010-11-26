1aad9e60f9f65b5907affb7cd08a916de97eaf39
Marco Antoniotti <marco.antoniotti@unimib.it>
2010-11-26 19:29:02 +0100

    Added file.

---
+++ utilities/text-utilities.lisp
@@ -0,0 +1,29 @@
+;;;; -*- Mode: Lisp -*-
+
+;;;; text-utilities.lisp --
+;;;; A few text utilities.
+
+(in-package "HELAMBDAP")
+
+(declaim (inline newlines newline))
+
+
+(defun newlines (&optional (out *standard-output*) (n 1))
+  (dotimes (i n) (terpri out)))
+
+
+(defun newline (&optional (out *standard-output*))
+  (newlines out 1))
+
+
+(defun empty-string-p (s)
+  (declare (type (or null string) s))
+  (and s (string= "" s)))
+
+
+(defun non-empty-string-p (s)
+  (declare (type (or null string) s))
+  (and s (not (null (string/= "" s)))))
+
+
+;;;; end of file -- text-utilities.lisp --
