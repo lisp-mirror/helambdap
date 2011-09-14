865d4ab4e88f4fa43beb8f062eb6421c3b24b7e9
Marco Antoniotti <marco.antoniotti@unimib.it>
2011-09-14 12:38:24 +0200

    Added files.

---
+++ utilities/functions-utilities.lisp
@@ -0,0 +1,11 @@
+;;;; -*- Mode: Lisp -*-
+
+;;;; functions-utilities.lisp --
+;;;; A few functional utilities (to avoid dependencies).
+;;;;
+;;;; See file COPYING for copyright and licensing information.
+
+(in-package "HELAMBDAP")
+
+
+;;;; end of file -- functions-utilities.lisp --
---
+++ utilities/time-utilities.lisp
@@ -0,0 +1,29 @@
+;;;; -*- Mode: Lisp -*-
+
+;;;; time-utilities.lisp --
+;;;; A few functional utilities (to avoid dependencies).
+;;;;
+;;;; See file COPYING for copyright and licensing information.
+
+(in-package "HELAMBDAP")
+
+(defconstant +months+
+  #("Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec"))
+
+(defconstant +weekdays+
+  #("Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun"))
+
+(defun text-timestamp ()
+  "Produces a string of th form \"Day Mon Dat HH:MM:SS TZ YEAR\"."
+  (multiple-value-bind (s mi h d mo y w dls z)
+      (get-decoded-time)
+    (declare (ignore dls))
+    (format nil "~A ~A ~D ~D:~D:~D GMT~D ~D"
+            (aref +weekdays+ w)
+            (aref +months+ mo)
+            d
+            h mi s
+            z
+            y)))
+    
+;;;; end of file -- time-utilities.lisp --
