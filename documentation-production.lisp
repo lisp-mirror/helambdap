6c655157eb3bf971d7897b90d60a4f921ce98157
Marco Antoniotti <marco.antoniotti@unimib.it>
2013-06-09 22:43:54 +0200

    Added top level switches for the DOCUMENT function.

--- documentation-production.lisp
+++ documentation-production.lisp
@@ -4,11 +4,25 @@
 
 (in-package "HELAMBDAP")
 
+;;;;===========================================================================
+;;;; Global special parameters controlling the documentation production.
+;;;;
+;;;; The *EVERYTHING*, *ONLY-DOCUMENTED*, *ONLY-EXPORTED* need the
+;;;; following explanation.  When *EVERYTHING* is true, HELAMBDAP produces
+;;;; documentation for every bit in the "search" path requested regardless
+;;;; of the values of *ONLY-DOCUMENTED* and *ONLY-EXPORTED*.
+
+(defparameter *supersede-documentation* t)
+
+(defparameter *everything* nil)
+
+(defparameter *only-documented* t)
+
+(defparameter *only-exported* nil)
+
 
 ;;;;===========================================================================
 ;;;; Protocol.
-
-(defparameter *supersede-documentation* t)
 
 (defgeneric produce-documentation (format element out doc-bits
                                           &key documentation-title
@@ -56,20 +70,36 @@ can be used as building blocks for the final documentation."))
                  (source #P"")
                  (destination 
                   (make-pathname :directory '(:relative "doc" "html")))
-                 (supersede *supersede-documentation*)
+
+                 ((:supersede *supersede-documentation*)
+                  *supersede-documentation*)
+                 ((:only-documented *only-documented*) *only-documented*)
+                 ((:only-exported *only-exported*) *only-exported*)
+                 ((:everything *everything*) *everything*)
+
+                 ((:exclude-directories *exclude-directories*)
+                  *exclude-directories*)
+                 ((:exclude-files *exclude-files*)
+                  *exclude-files*)
+                 
                  &allow-other-keys
                  )
-  "Produces the documetation for something.
+  "Produces the documentation for something.
 
 The function is a wrapper for BUILD-DOCUMENTATION defaulting a few
 parameters, in particular the output FORMAT (which defaults to HTML)."
-  (let ((*supersede-documentation* supersede))
-    (build-documentation for-what
-                         format
-                         :layout layout
-                         :source source
-                         :destination destination
-                         :documentation-title documentation-title)))
+  
+  (when (and *everything* (or *only-documented* *only-exported*))
+    (warn "EVERYTHNG is currently true: HELAMBDAP will produce all ~@
+           documentation files regardless of the value of other 'limiting' ~@
+           variables."))
+
+  (build-documentation for-what
+                       format
+                       :layout layout
+                       :source source
+                       :destination destination
+                       :documentation-title documentation-title))
 
 
 ;;;;---------------------------------------------------------------------------
