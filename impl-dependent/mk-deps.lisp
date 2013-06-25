a45adc4edbb02970c6e6c864a537054e782608c0
Marco Antoniotti <marco.antoniotti@unimib.it>
2013-06-25 11:49:01 -0400

    File added.

---
+++ impl-dependent/mk-deps.lisp
@@ -0,0 +1,11 @@
+;;;; -*- Mode: Lisp -*-
+
+;;;; mk-deps.lisp --
+;;;; XHTMLambda ASDF dependent code.
+;;;;
+;;;; Copyright (c) 2013, Marco Antoniotti
+;;;; See file COPYING for more information.
+
+(in-package "MAKE")
+
+;;;; mk-deps.lisp --
