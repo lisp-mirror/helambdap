e5e107426238ae0184affb78cbfe1bccce8f202d
Marco Antoniotti <marco.antoniotti@unimib.it>
2013-11-08 17:16:07 +0100

    Added functionality to deal with ASDF files list.
    Thanks to Faré for the ASDF2 and ASDF3 suggestions.
    All in all document production by HELambdaP via systems is not quite
    fully tested, but it should be there RSN(tm).

--- impl-dependent/asdf-deps.lisp
+++ impl-dependent/asdf-deps.lisp
@@ -3,13 +3,15 @@
 ;;;; asdf-deps.lisp --
 ;;;; XHTMLambda ASDF dependent code.
 ;;;;
-;;;; Copyright (c) 2013, Marco Antoniotti
 ;;;; See file COPYING for more information.
+
 
 (in-package "ASDF")
 
-(export '(files-in-system))
+#+asdf2
+(export '(files-in-system) "ASDF")
 
+#+asdf2
 (defun files-in-system (s)
   (check-type s (or string symbol asdf:system))
 
@@ -33,5 +35,36 @@
       (collect-filenames s)
       (nreverse filenames))))
 
+
+;;;===========================================================================
+;;; Thanks to Fare` for these...
+
+#+asdf3
+(export '(system-input-files system-files) "ASDF")
+
+#+asdf3
+(defun system-input-files (system)
+  (multiple-value-bind (i o) 
+      (while-collecting (i o)
+        (loop for (op . comp)
+              in (plan-actions
+                  (traverse-sub-actions 'load-op (find-system system)))
+              do (map () #'i (input-files op comp))
+              (map () #'o (output-files op comp))))
+    (remove-if #'(lambda (f) (member f o :test 'pathname-equal))
+               (remove-duplicates i :from-end t :test 'pathname-equal))))
+
+;;; Of course, you could pray that no one is doing anything funky with
+;;; input-files, and instead use the simpler:
+
+#+asdf3
+(defun system-files (system)
+  (cons (system-source-file system)
+        (mapcar 'component-pathname 
+                (remove-duplicates
+                 (required-components system
+                                      :other-systems nil
+                                      :component-type '(not system)) 
+                 :from-end t))))
 
 ;;;; asdf-deps.lisp --
