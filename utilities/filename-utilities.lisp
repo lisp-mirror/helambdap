edbdf866ef171ed3cb92c9176fe540554354613d
Marco Antoniotti <marco.antoniotti@unimib.it>
2015-12-05 19:51:09 +0100

    Added ENCLOSING-DIRECTORY function.

--- utilities/filename-utilities.lisp
+++ utilities/filename-utilities.lisp
@@ -9,7 +9,7 @@
 
 
 (defun base-name (file-n-or-p &optional (type nil)
-                             &aux (file-pathname (pathname file-n-or-p)))
+                              &aux (file-pathname (pathname file-n-or-p)))
   (declare (type (or string pathname) file-n-or-p)
            (type (or null string) type)
            (type pathname file-pathname))
@@ -34,6 +34,36 @@
                   :name nil
                   :type nil
                   :defaults file-pathname)))
+
+
+(defun enclosing-directory (file-n-or-p &aux (file-pathname (pathname file-n-or-p)))
+  (declare (type (or string pathname) file-n-or-p)
+           (type pathname file-pathname))
+  (let* ((tn file-pathname)
+         (tnd (pathname-directory tn))
+         (tnn (pathname-name tn))
+         (tnt (pathname-type tn))
+         )
+    (flet ((get-directory (dir n &aux (l (length dir)))
+             (when (and dir (listp dir))
+               (cond ((> l 2)
+                      (cons :relative (last dir (min (- l 2) n))))
+                     ((= l 2)
+                      (if (eq :absolute (first dir))
+                          dir
+                          (list :relative (second dir))))
+                     (t dir))))
+           )
+                     
+      (if (or (and (null tnn) (null tnt))
+              (and (eq tnn :unspecific) (eq tnt :unspecific))
+              (and (null tnn) (eq tnt :unspecific))
+              (and (eq tnn :unspecific) (null tnt)))
+          ;; Pathname names - most likely, these being CL pathnames - a directory.
+          (get-directory tnd 2)
+          ;; Pathname names a regular file.
+          (get-directory tnd 1)
+          ))))
 
 
 (defun quote-wild-in-pathname-name (name) ; Can't use SUBSTITUTE.
