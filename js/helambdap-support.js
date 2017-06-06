6ff275c118398d2b6dc6c716127eddc4b5e85faf
Marco Antoniotti <marco.antoniotti@unimib.it>
2017-06-06 09:28:10 +0200

    Added file.

---
+++ js/helambdap-support.js
@@ -0,0 +1,54 @@
+// -*- Mode: JavaScript -*-
+
+// helambdap-support.js --
+// Javascript support code for HELambdaP.
+// This code is included as a <script> in each top-level HTML5
+// HELambdaP-generated file.
+//
+// 20170504 MA: New version.
+//
+// See file COPYING in top level folder for licence and copying
+// information.
+
+var hlp_xhttp = new XMLHttpRequest();
+
+
+// function set_nav_mode(mode) {
+//     str = '<div id="nav_index"></div>';
+//     if (mode === 1) {
+// 	str = '<div id="nav_map"></div><div id="nav_list"></div>';
+//     }
+//     document.getElementById('nav').innerHTML = str;
+    
+//     str = '<div id=\"introduction_frame\"></div>';
+//     if (mode === 1) {
+// 	str = '<div id="dictionary-entries_frame"></div>';
+//     }
+//     document.getElementById('main').innerHTML = str;
+// };
+
+
+function hlp_load_section(id, filename) {
+    hlp_xhttp.open("GET", filename, false); 
+    hlp_xhttp.send();
+    document.getElementById(id).innerHTML =
+	hlp_xhttp.responseText;
+};
+
+
+function hlp_load_introduction() {
+    // set_nav_mode(0);
+    hlp_load_section('mainnav', 'introduction-navigation.html');
+    hlp_load_section('main', 'introduction.html');
+};
+
+
+function hlp_load_dictionary() {
+    hlp_load_section('mainnav',
+		     'dictionary/dictionary-navigation.html');
+    
+    hlp_load_section('main',
+		     'dictionary/dictionary-entries.html')
+};
+
+// end of file -- helambdap-js-support.js
