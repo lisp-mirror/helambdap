502f85d2ccfd533658a2364f1c9a20a41bf536a5
Marco Antoniotti <marco.antoniotti@unimib.it>
2018-05-15 23:13:03 +0200

    Started adding some package related conditions.

--- impl-dependent/lispworks.lisp
+++ impl-dependent/lispworks.lisp
@@ -6,4 +6,30 @@
 
 (editor:setup-indent "with-html-syntax" 1 4 6)
 
-;;;; lispworks.lisp --
+
+;;;; HELambdaP Lispworks dependent code.
+
+(in-package "HLP")
+
+(define-condition unknown-package-lw (unknown-package
+                                      conditions:package-not-found)
+  ()
+  )
+
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (setf *unknown-package-condition-class* 'unknown-package-lw))
+
+
+(defmethod make-unknown-package-condition
+           ((upc-class (eql 'unknown-package-lw)))
+  (make-instance 'unknown-package-lw))
+
+
+(defmethod make-unknown-package-condition
+           ((pnf conditions:package-not-found))
+  (change-class pnf
+                'unknown-package-lw
+                :name (package-error-package pnf)))
+
+;;;; end of file -- lispworks.lisp
