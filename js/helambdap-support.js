9c80d6f89c1c63125a9e4bd1feca4fd1106bbe0c
Marco Antoniotti <marco.antoniotti@unimib.it>
2019-02-26 20:49:38 -0500

    New Javascript code based on the Fetch API.

--- js/helambdap-support.js
+++ js/helambdap-support.js
@@ -14,67 +14,6 @@
 //              work around CORS.
 
 
-// function set_nav_mode(mode) {
-//     str = '<div id="nav_index"></div>';
-//     if (mode === 1) {
-// 	str = '<div id="nav_map"></div><div id="nav_list"></div>';
-//     }
-//     document.getElementById('nav').innerHTML = str;
-    
-//     str = '<div id=\"introduction_frame\"></div>';
-//     if (mode === 1) {
-// 	str = '<div id="dictionary-entries_frame"></div>';
-//     }
-//     document.getElementById('main').innerHTML = str;
-// };
-
-
-function hlp_load_section(id, filename) {
-    var hlp_xhttp = new XMLHttpRequest();
-
-    // hlp_xhttp.responseType = 'text';
-    // hlp_xhttp.open("GET", filename, false); 
-    // hlp_xhttp.send();
-    // document.getElementById(id).innerHTML = hlp_xhttp.responseText;
-    
-    console.log('>>> ' + filename + '\n    ' + document.URL);
-    document.getElementById(id).innerHTML =
-	filename + '<br />' + document.URL;
-    return;
-};
-
-
-function hlp_load_introduction() {
-    // set_nav_mode(0);
-    // hlp_load_section('mainnav', document.URL + 'introduction-navigation.html');
-    // hlp_load_section('main', document.URL + 'introduction.html');
-    // hlp_get_section('main', document.URL + 'introduction.html');
-    hlp_get_section('main', 'introduction.html');
-    hlp_get_section('mainnav', 'introduction-navigation.html');
-    return;
-};
-
-
-function hlp_load_dictionary() {
-    // hlp_load_section('mainnav',
-    // 		     // 'dictionary/dictionary-navigation.html'
-    // 		     'dictionary/dictionary-navigation-map.html'
-    // 		    );
-    
-    // hlp_load_section('main',
-    // 		     'dictionary/dictionary-entries.html');
-
-    hlp_get_section('mainnav',
-		    // 'dictionary/dictionary-navigation.html'
-		    'dictionary/dictionary-navigation-map.html'
-		   );
-    
-    hlp_get_section('main',
-		    'dictionary/dictionary-entries.html');
-    return;
-};
-
-
 function hlp_get_section(id, filename) {
     console.log('>>> ' + id + '; ' + filename);
     console.log('>>> ' + id + '; ' + filename.substring(0, filename.lastIndexOf('/')));
@@ -104,7 +43,23 @@ function hlp_get_section(id, filename) {
 		document.createTextNode('Error: ' + error.message)
             );
 	});
-}
+    return;
+};
 
 
-// end of file -- helambdap-js-support.js
+function hlp_load_introduction() {
+    hlp_get_section('main', 'introduction.html');
+    hlp_get_section('mainnav', 'introduction-navigation.html');
+    return;
+};
+
+
+function hlp_load_dictionary() {
+    hlp_get_section('main',
+		    'dictionary/dictionary-entries.html');
+    hlp_get_section('mainnav',
+		    'dictionary/dictionary-navigation-map.html');
+    return;
+};
+
+// end of file -- helambdap-support.js
