c456387ea6e156685c71ad2773b3fced90768f23
Marco Antoniotti <marco.antoniotti@unimib.it>
2020-04-30 00:57:01 +0200

    Added a few features...

--- utilities/debugging-utilities.lisp
+++ utilities/debugging-utilities.lisp
@@ -5,7 +5,12 @@
 ;;;; This will evolve in the tool "Never Underestimate The Debugging
 ;;;; Power of Print" (NUTDPP).
 
-(defparameter *debug-output* *debug-io*)
+; (define-symbol-macro *debug-output* *debug-io*) ; It has to be so.
+
+(defvar *debug-output* *error-output*) ; For the time being; error
+                                       ; otherwise because of variable
+                                       ; redefinition as a symbol-macro.
+
 
 ;;; *debug-tag*
 ;;; Controls when and how to print debugging information.
@@ -45,6 +50,25 @@
   (output *debug-output* :type stream))
 
 
+(defstruct debug-log-channel
+  (name :all :type symbol)
+  (stack () :type list))
+
+
+(defvar *debug-log-channels* (make-hash-table))
+
+
+(defun open-log-channel (name)
+  (setf (gethash name *debug-log-channels*)
+        (make-debug-log-channel)))
+
+
+(defun close-log-channel (name)
+  (remhash name *debug-log-channels*))
+  
+
+
+
 (defun debugmsg (tag prefix format-control &rest format-arguments)
   "Prints/logs a message for debugging on *DEBUG-OUTPUT*.
 
@@ -58,10 +82,10 @@ message.
              (or (and (numberp tag)
                       (numberp *current-debug-tag*)
                       (plusp tag)
-                      (or (<= +max-debug-tab+ tag)
+                      (or (<= +max-debug-tag+ tag)
                           (<= *current-debug-tag* tag)))
                  (eq tag t)
-                 (equal tag *debug-tag*)
+                 (equal tag *current-debug-tag*)
                  (find tag *registered-debug-tags* :test #'equal)))
 
     (cond ((eq prefix t)
@@ -112,9 +136,11 @@ message.
 (defun start-debug-logging (&key (prefix *current-prefix*)
                                  (tag *current-debug-tag*)
                                  (output *debug-output*)
+                                 (channel :all)
                                  )
+  (declare (ignore channel)) ; Later.
   (let ((ndc (make-debug-log-context :prefix prefix
-                                     :tag tag
+                                     :current-tag tag
                                      :output output)))
     (push ndc *%%debug-logging-stack%%*)
     (setf *current-prefix* prefix
@@ -124,16 +150,14 @@ message.
     ))
 
 
-(defun end-debugging-logging ()
+(defun end-debugging-logging (&key (channel :all))
+  (declare (ignore channel)) ; Later.
   (pop *%%debug-logging-stack%%*)
   (let ((dlc (first *%%debug-logging-stack%%*)))
     (when dlc ; It should never be NIL, but...
       (setf *current-prefix* (debug-log-context-prefix dlc)
-            *current-debug-tag* (debug-log-context dlc)
+            *current-debug-tag* (debug-log-context-current-tag dlc)
             *debug-output* (debug-log-context-output dlc)))
     dlc))
-  
-
-
 
 ;;;; end of file -- debugging-utilities.lisp
